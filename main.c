c2d20ab4f73bcc15a263b8989e0ec4c7cc23deeb
Charlie <charliegeorgehill0@gmail.com>
2023-06-28 22:55:59 +0100

    Add Waveshare e-Paper libs

--- main.c
+++ main.c
@@ -1,8 +1,32 @@
+#include <stdio.h>
 #include "pico/stdlib.h"
 #include "pico/cyw43_arch.h"
+#include "hardware/adc.h"
 
-int main() {
+#include "DEV_Config.h"
+#include "GUI_Paint.h"
+#include "Debug.h"
+#include "EPD_2in13_V3.h"
+
+float read_onboard_temperature() {
+    
+    /* 12-bit conversion, assume max value == ADC_VREF == 3.3 V */
+    const float conversionFactor = 3.3f / (1 << 12);
+
+    float adc = (float)adc_read() * conversionFactor;
+    float tempC = 27.0f - (adc - 0.706f) / 0.001721f;
+
+    return tempC;
+}
+
+
+int main(void) {
     stdio_init_all();
+
+    adc_init();
+    adc_set_temp_sensor_enabled(true);
+    adc_select_input(4);
+
     if (cyw43_arch_init()) {
         printf("Wi-Fi init failed");
         return -1;
@@ -12,6 +36,8 @@ int main() {
         sleep_ms(250);
         cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 0);
         sleep_ms(250);
-        printf("Flashing");
+
+        float temperature = read_onboard_temperature();
+        printf("Onboard temperature = %.02f C\n", temperature);
     }
 }
---
+++ plan.md
@@ -0,0 +1,22 @@
+<!-- Loading Code -->
+- Initialise display, GPS & WiFI
+- Update display as loading happens
+
+<!-- Main Code -->
+- Main loop to make calls to the modules
+
+<!-- Display Code -->
+- Connect the display
+- Screens (Loading, Main, Status)
+- Main:
+    - Speed & MPH
+    - Icons at bottom of screen
+    - Temperature
+    - Time
+
+<!-- GPS Code -->
+- Fetch position
+- Calculate MPH speed
+- Time?
+
+<!-- HTTP Calls -->
